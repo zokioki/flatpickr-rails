f518799aab8ead4cbfc2aec5a7e2109d8fcfb382
Zoran Pesic <zoran1991@gmail.com>
2020-03-21 01:15:07 -0700

    update rake task to pull from npm package, update assets to package version

--- vendor/assets/javascripts/flatpickr.js
+++ vendor/assets/javascripts/flatpickr.js
@@ -1,4 +1,4 @@
-/* flatpickr v4.6.2, @license MIT */
+/* flatpickr v4.6.3, @license MIT */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
